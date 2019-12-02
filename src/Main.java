d4d1f3063b22c3612571b1e1811914abca24630a
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2019-12-02 16:20:21 +0200

    Created Bantam and Austin_Martin_Valkrie object from Bakkie and RaceCar class respectively

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
-    public void main(String[] args){
+    public static void main(String[] args){
+        //Created the Bantam object from Bakkie class
+       Bakkie Bantam = new Bakkie("Red");
+       Bantam.drive();
+       System.out.println("Colour of car: "+Bantam.colour +", Position: "+ Bantam.position);
+
+       //Created the Austine_Martic_Valkyrie object from the Race Car class
+        RaceCar Austine_Martic_Valkyrie = new RaceCar("Silver",20);
+        Austine_Martic_Valkyrie.drive();
+        System.out.println("Colour of car "+Austine_Martic_Valkyrie.colour+" ,Position"+Austine_Martic_Valkyrie.position);
+
 
     }
 }
