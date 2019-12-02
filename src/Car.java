d26333076dc982a00daf10f6d5bc750134c9ca5b
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2019-12-02 15:42:09 +0200

    Created the base class called Car with attributes and constructor

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    //Class attributes that are accessible to the base class
+    protected String colour;
+    protected int position;
+
+    //Constructor
+    public Car(String colour,int position){
+        this.colour =colour;
+        this.position = position;
+
+    }
+
+}
